024a51bce76f2746335b898e15c58ab84a26830a
mobicham <37179323+mobicham@users.noreply.github.com>
2023-11-24 10:13:11 +0100

    Update setup.py - update dependencies

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     author_email='hicham@mobiuslabs.com',
     license='Apache 2',
     packages=['hqq', 'hqq/models', 'hqq/core'],
-    install_requires=['numpy>=1.24.4','tqdm>=4.64.1', 'torch>=2.0.1', 'huggingface_hub'],
+    install_requires=['numpy>=1.24.4','tqdm>=4.64.1', 'torch>=2.0.1', 'huggingface_hub', 'accelerate', 'timm', 'transformers[torch]'],
 )
