cb3d66b5f1cea48afdbb5efe95391bdaf900b75e
mobicham <37179323+mobicham@users.noreply.github.com>
2023-12-01 17:29:38 +0100

    Update setup.py - alpha

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hqq',
-    version='1.0.0',    
+    version='0.1.alpha',    
     description='Half-Quadratic Quantization (HQQ)',
     url='https://github.com/mobiusml/hqq/tree/main/code',
     author='Dr. Hicham Badri',
