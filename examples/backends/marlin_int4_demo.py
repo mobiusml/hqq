d965ce6fbd484b4d09a9166ff299dd2e72ebd87d
mobicham <hicham@mobiuslabs.com>
2024-04-19 12:21:57 +0000

    add marlin int4 example

---
+++ examples/backends/marlin_int4_demo.py
@@ -0,0 +1,45 @@
+# pip install git+https://github.com/mobiusml/hqq.git;
+# pip install git+https://github.com/IST-DASLab/marlin.git;
+# num_threads=12; OMP_NUM_THREADS=$num_threads CUDA_VISIBLE_DEVICES=0 ipython3 
+##########################################################################################################################################################
+import torch, os
+
+os.environ["TOKENIZERS_PARALLELISM"]  = "1"
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.allow_tf32       = True
+
+cache_path     = '.'
+model_id       = "meta-llama/Llama-2-7b-chat-hf"
+compute_dtype  = torch.float16 #int4 kernel only works with float16
+device         = 'cuda:0'
+
+##########################################################################################################################################################
+from hqq.engine.hf import HQQModelForCausalLM, AutoTokenizer
+from hqq.core.quantize import *
+
+tokenizer    = AutoTokenizer.from_pretrained(model_id, cache_dir=cache_path)
+model        = HQQModelForCausalLM.from_pretrained(model_id, cache_dir=cache_path, torch_dtype=compute_dtype, attn_implementation="sdpa")
+quant_config = BaseQuantizeConfig(nbits=4, group_size=None, quant_scale=False, quant_zero=False, axis=1)
+
+model.quantize_model(quant_config=quant_config, compute_dtype=compute_dtype, device=device)
+
+#Set default backends, to compare with int4mm
+if(quant_config['weight_quant_params']['axis']==0):
+    HQQLinear.set_backend(HQQBackend.ATEN)
+else:
+    HQQLinear.set_backend(HQQBackend.PYTORCH)
+
+##########################################################################################################################################################
+
+#Replace HQQLinear layers matmuls to support int4 mm
+from hqq.utils.patching import prepare_for_inference
+prepare_for_inference(model, use_marlin=True)
+
+#Import custom HF generator
+from hqq.utils.generation_hf import HFGenerator
+
+#Generate
+gen = HFGenerator(model, tokenizer, do_sample=True, compile_args=None) #skips compilation: slower, but works properly
+#gen = HFGenerator(model, tokenizer, do_sample=True) #compiled: much faster, but there's a bug with HF's StaticCache 
+
+out = gen.generate("Write an essay about large language models.", max_new_tokens=1000, print_tokens=True)
