8cc5c5ea11b3a03a7a23896b51852459e7210232
root <hicham@mobiuslabs.com>
2024-08-20 12:11:57 +0000

    __restrict__ and cache read-only tensors

--- hqq/kernels/hqq_aten_cuda_kernel.cu
+++ hqq/kernels/hqq_aten_cuda_kernel.cu
@@ -28,9 +28,9 @@ using namespace at;
 /*******************************************************************************************************************************************/
 //Simple
 template <typename scalar_t>
-__global__ void dequantize_8bit_u8_kernel(unsigned char* Wq_packed, scalar_t* scale, scalar_t* zero, scalar_t* W_r, int h, int w) { 
+__global__ void dequantize_8bit_u8_kernel(unsigned char* __restrict__ Wq_packed, scalar_t* __restrict__ scale, scalar_t* __restrict__ zero, scalar_t* W_r, int h, int w) { 
 	int i = blockIdx.x*blockDim.x + threadIdx.x;
-	int n = h*w;
+	const int n = h*w;
 	if(i>=n) return;
 
 	int j   = i % w;
@@ -65,8 +65,6 @@ torch::Tensor dequantize_8bit_u8(torch::Tensor &Wq_packed, torch::Tensor &scale,
 
 
 	C10_CUDA_KERNEL_LAUNCH_CHECK();
-	//cudaDeviceSynchronize();
-
 	return W_r;
 }
 
@@ -75,12 +73,13 @@ torch::Tensor dequantize_8bit_u8(torch::Tensor &Wq_packed, torch::Tensor &scale,
 /*******************************************************************************************************************************************/
 
 //Simple
-__global__ void unpack_4bit_u8_kernel(unsigned char* Wq_packed, unsigned char* Wq_unpacked, int n) { 
+__global__ void unpack_4bit_u8_kernel(unsigned char* __restrict__ Wq_packed, unsigned char* Wq_unpacked, int n) { 
 	int i = blockIdx.x*blockDim.x + threadIdx.x;
 	if(i>=n) return;
 
-	Wq_unpacked[i]     = (Wq_packed[i] & 0xF0) >> 4;  //First chunk
-	Wq_unpacked[i + n] = (Wq_packed[i] & 0x0F);       //Second chunk	
+	const unsigned char _Wq  = Wq_packed[i];
+	Wq_unpacked[i]     = (_Wq & 0xF0) >> 4;  //First chunk
+	Wq_unpacked[i + n] = (_Wq & 0x0F);       //Second chunk	
 }
 
 torch::Tensor unpack_4bit_u8(torch::Tensor &Wq_packed)
@@ -98,21 +97,23 @@ torch::Tensor unpack_4bit_u8(torch::Tensor &Wq_packed)
 	unpack_4bit_u8_kernel<<<blocks, BLOCK_SIZE>>>(Wq_packed.data_ptr<unsigned char>(), Wq_unpacked.data_ptr<unsigned char>(), n);	
 
 	C10_CUDA_KERNEL_LAUNCH_CHECK();
-	//cudaDeviceSynchronize();
-	
 	return Wq_unpacked;
 }
 
 //Simple
 template <typename scalar_t>
-__global__ void dequantize_4bit_u8_kernel(unsigned char* Wq_packed, scalar_t* scale, scalar_t* zero, scalar_t* W_r, int h, int w) { 
+__global__ void dequantize_4bit_u8_kernel(unsigned char* __restrict__ Wq_packed, scalar_t* __restrict__ scale, scalar_t* __restrict__ zero, scalar_t* W_r, int h, int w) { 
 	int i = blockIdx.x*blockDim.x + threadIdx.x;
-	int n = h*w;
+	const int n = h*w;
 	if(i>=n) return;
 
-	int j      = i % w;
-	W_r[i]     = (scalar_t((Wq_packed[i] & 0xF0) >> 4) - zero[j])*scale[j];  //First chunk
-	W_r[i + n] = (scalar_t((Wq_packed[i] & 0x0F))      - zero[j])*scale[j];  //Second chunk
+	const int j             = i % w;
+	const unsigned char _Wq = Wq_packed[i];
+	const scalar_t _zero    = zero[j];
+	const scalar_t _scale   = scale[j];
+
+	W_r[i]     = (scalar_t((_Wq & 0xF0) >> 4) - _zero)*_scale;  //First chunk
+	W_r[i + n] = (scalar_t((_Wq & 0x0F))      - _zero)*_scale;  //Second chunk
 }
 
 torch::Tensor dequantize_4bit_u8(torch::Tensor &Wq_packed, torch::Tensor &scale, torch::Tensor &zero)
@@ -142,10 +143,7 @@ torch::Tensor dequantize_4bit_u8(torch::Tensor &Wq_packed, torch::Tensor &scale,
 
 
 	C10_CUDA_KERNEL_LAUNCH_CHECK();
-	//cudaDeviceSynchronize();
-
 	return W_r;
-
 }
 
 /*******************************************************************************************************************************************/
@@ -153,14 +151,16 @@ torch::Tensor dequantize_4bit_u8(torch::Tensor &Wq_packed, torch::Tensor &scale,
 /*******************************************************************************************************************************************/
 
 //Simple
-__global__ void unpack_2bit_u8_kernel(unsigned char* Wq_packed, unsigned char* Wq_unpacked, int n) { 
+__global__ void unpack_2bit_u8_kernel(unsigned char* __restrict__ Wq_packed, unsigned char* Wq_unpacked, int n) { 
 	int i = blockIdx.x*blockDim.x + threadIdx.x;
 	if(i>=n) return;
 
-	Wq_unpacked[i]       = (Wq_packed[i] & 0xC0) >> 6;  //1st chunk
-	Wq_unpacked[i + n]   = (Wq_packed[i] & 0x30) >> 4;  //2nd chunk
-	Wq_unpacked[i + n*2] = (Wq_packed[i] & 0x0C) >> 2;  //3rd chunk	
-	Wq_unpacked[i + n*3] = (Wq_packed[i] & 0x03);       //4th chunk	
+	const unsigned char _Wq = Wq_packed[i];
+
+	Wq_unpacked[i]       = (_Wq & 0xC0) >> 6;  //1st chunk
+	Wq_unpacked[i + n]   = (_Wq & 0x30) >> 4;  //2nd chunk
+	Wq_unpacked[i + n*2] = (_Wq & 0x0C) >> 2;  //3rd chunk	
+	Wq_unpacked[i + n*3] = (_Wq & 0x03);       //4th chunk	
 }
 
 
@@ -179,51 +179,26 @@ torch::Tensor unpack_2bit_u8(torch::Tensor &Wq_packed)
 	unpack_2bit_u8_kernel<<<blocks, BLOCK_SIZE>>>(Wq_packed.data_ptr<unsigned char>(), Wq_unpacked.data_ptr<unsigned char>(), n);	
 
 	C10_CUDA_KERNEL_LAUNCH_CHECK();
-	//cudaDeviceSynchronize();
-	
 	return Wq_unpacked;
 }
 
 //Simple
 template <typename scalar_t>
-__global__ void dequantize_2bit_u8_kernel(unsigned char* Wq_packed, scalar_t* scale, scalar_t* zero, scalar_t* W_r, int h, int w) { 
+__global__ void dequantize_2bit_u8_kernel(unsigned char* __restrict__ Wq_packed, scalar_t* __restrict__ scale, scalar_t* __restrict__ zero, scalar_t* W_r, int h, int w) { 
 	int i = blockIdx.x*blockDim.x + threadIdx.x;
-	int n = h*w;
+	const int n = h*w;
 	if(i>=n) return;
 
-	int j        = i % w;
-	W_r[i]       = (scalar_t((Wq_packed[i] & 0xC0) >> 6) - zero[j])*scale[j];  //1st chunk
-	W_r[i + n]   = (scalar_t((Wq_packed[i] & 0x30) >> 4) - zero[j])*scale[j];  //2nd chunk
-	W_r[i + n*2] = (scalar_t((Wq_packed[i] & 0x0C) >> 2) - zero[j])*scale[j];  //3rd chunk	
-	W_r[i + n*3] = (scalar_t((Wq_packed[i] & 0x03))      - zero[j])*scale[j];  //4th chunk	
+	const int j             = i % w;
+	const unsigned char _Wq = Wq_packed[i];
+	const scalar_t _zero    = zero[j];
+	const scalar_t _scale   = scale[j];
+
+	W_r[i]       = (scalar_t((_Wq & 0xC0) >> 6) - _zero)*_scale;  //1st chunk
+	W_r[i + n]   = (scalar_t((_Wq & 0x30) >> 4) - _zero)*_scale;  //2nd chunk
+	W_r[i + n*2] = (scalar_t((_Wq & 0x0C) >> 2) - _zero)*_scale;  //3rd chunk	
+	W_r[i + n*3] = (scalar_t((_Wq & 0x03))      - _zero)*_scale;  //4th chunk	
 }
-
-
-// //Shared
-// template <typename scalar_t>
-// __global__ void dequantize_2bit_u8_kernel(unsigned char* Wq_packed, scalar_t* scale, scalar_t* zero, scalar_t* W_r, int h, int w) { 
-// 	int i = blockIdx.x*blockDim.x + threadIdx.x;
-// 	int n = h*w;
-// 	int s = threadIdx.x;
-
-// 	if(i>=n) return;
-
-// 	__shared__ unsigned char shared[BLOCK_SIZE];
-// 	__shared__ scalar_t shared_meta[BLOCK_SIZE][2];
-	
-// 	int j             = i % w;
-// 	shared[s]         = Wq_packed[i];
-// 	shared_meta[s][0] = zero[j];
-// 	shared_meta[s][1] = scale[j];
-// 	__syncthreads();
-
-
-// 	W_r[i]       = (scalar_t((shared[s] & 0xC0) >> 6) - shared_meta[s][0])*shared_meta[s][1];  //1st chunk
-// 	W_r[i + n]   = (scalar_t((shared[s] & 0x30) >> 4) - shared_meta[s][0])*shared_meta[s][1];  //2nd chunk
-// 	W_r[i + n*2] = (scalar_t((shared[s] & 0x0C) >> 2) - shared_meta[s][0])*shared_meta[s][1];  //3rd chunk	
-// 	W_r[i + n*3] = (scalar_t((shared[s] & 0x03))      - shared_meta[s][0])*shared_meta[s][1];  //4th chunk	
-// }
-
 
 
 torch::Tensor dequantize_2bit_u8(torch::Tensor &Wq_packed, torch::Tensor &scale, torch::Tensor &zero)
@@ -253,8 +228,6 @@ torch::Tensor dequantize_2bit_u8(torch::Tensor &Wq_packed, torch::Tensor &scale,
 
 
 	C10_CUDA_KERNEL_LAUNCH_CHECK();
-	//cudaDeviceSynchronize();
-
 	return W_r;
 }
 
@@ -263,18 +236,20 @@ torch::Tensor dequantize_2bit_u8(torch::Tensor &Wq_packed, torch::Tensor &scale,
 /*******************************************************************************************************************************************/
 
 //Simple
-__global__ void unpack_1bit_u8_kernel(unsigned char* Wq_packed, unsigned char* Wq_unpacked, int n) { 
+__global__ void unpack_1bit_u8_kernel(unsigned char* __restrict__ Wq_packed, unsigned char* Wq_unpacked, int n) { 
 	int i = blockIdx.x*blockDim.x + threadIdx.x;
 	if(i>=n) return;
 
-	Wq_unpacked[i]       = (Wq_packed[i] & 0x80) >> 7;  //1st chunk
-	Wq_unpacked[i + n]   = (Wq_packed[i] & 0x40) >> 6;  //2nd chunk
-	Wq_unpacked[i + n*2] = (Wq_packed[i] & 0x20) >> 5;  //3rd chunk	
-	Wq_unpacked[i + n*3] = (Wq_packed[i] & 0x10) >> 4;  //4th chunk	
-	Wq_unpacked[i + n*4] = (Wq_packed[i] & 0x08) >> 3;  //5th chunk	
-	Wq_unpacked[i + n*5] = (Wq_packed[i] & 0x04) >> 2;  //6th chunk	
-	Wq_unpacked[i + n*6] = (Wq_packed[i] & 0x02) >> 1;  //7th chunk	
-	Wq_unpacked[i + n*7] = (Wq_packed[i] & 0x01);       //8th chunk	
+	const unsigned char _Wq = Wq_packed[i];
+
+	Wq_unpacked[i]       = (_Wq & 0x80) >> 7;  //1st chunk
+	Wq_unpacked[i + n]   = (_Wq & 0x40) >> 6;  //2nd chunk
+	Wq_unpacked[i + n*2] = (_Wq & 0x20) >> 5;  //3rd chunk	
+	Wq_unpacked[i + n*3] = (_Wq & 0x10) >> 4;  //4th chunk	
+	Wq_unpacked[i + n*4] = (_Wq & 0x08) >> 3;  //5th chunk	
+	Wq_unpacked[i + n*5] = (_Wq & 0x04) >> 2;  //6th chunk	
+	Wq_unpacked[i + n*6] = (_Wq & 0x02) >> 1;  //7th chunk	
+	Wq_unpacked[i + n*7] = (_Wq & 0x01);       //8th chunk	
 }
 
 torch::Tensor unpack_1bit_u8(torch::Tensor &Wq_packed)
@@ -291,56 +266,31 @@ torch::Tensor unpack_1bit_u8(torch::Tensor &Wq_packed)
 	int blocks = cdiv(n, BLOCK_SIZE);
 	unpack_1bit_u8_kernel<<<blocks, BLOCK_SIZE>>>(Wq_packed.data_ptr<unsigned char>(), Wq_unpacked.data_ptr<unsigned char>(), n);	
 
-	C10_CUDA_KERNEL_LAUNCH_CHECK();
-	//cudaDeviceSynchronize();
-	
+	C10_CUDA_KERNEL_LAUNCH_CHECK();	
 	return Wq_unpacked;
 }
 
 //Simple
 template <typename scalar_t>
-__global__ void dequantize_1bit_u8_kernel(unsigned char* Wq_packed, scalar_t* scale, scalar_t* zero, scalar_t* W_r, int h, int w) { 
+__global__ void dequantize_1bit_u8_kernel(unsigned char* __restrict__ Wq_packed, scalar_t* __restrict__ scale, scalar_t* __restrict__ zero, scalar_t* W_r, int h, int w) { 
 	int i = blockIdx.x*blockDim.x + threadIdx.x;
-	int n = h*w;
+	const int n = h*w;
 	if(i>=n) return;
 
-	int j        = i % w;
-	W_r[i]       = (scalar_t((Wq_packed[i] & 0x80) >> 7) - zero[j])*scale[j];  //1st chunk
-	W_r[i + n]   = (scalar_t((Wq_packed[i] & 0x40) >> 6) - zero[j])*scale[j];  //2nd chunk
-	W_r[i + n*2] = (scalar_t((Wq_packed[i] & 0x20) >> 5) - zero[j])*scale[j];  //3rd chunk	
-	W_r[i + n*3] = (scalar_t((Wq_packed[i] & 0x10) >> 4) - zero[j])*scale[j];  //4th chunk	
-	W_r[i + n*4] = (scalar_t((Wq_packed[i] & 0x08) >> 3) - zero[j])*scale[j];  //5th chunk	
-	W_r[i + n*5] = (scalar_t((Wq_packed[i] & 0x04) >> 2) - zero[j])*scale[j];  //6th chunk	
-	W_r[i + n*6] = (scalar_t((Wq_packed[i] & 0x02) >> 1) - zero[j])*scale[j];  //7th chunk	
-	W_r[i + n*7] = (scalar_t((Wq_packed[i] & 0x01))      - zero[j])*scale[j];  //8th chunk	
+	const int j              = i % w;
+	const unsigned char _Wq  = Wq_packed[i];
+	const scalar_t     _zero = zero[j];
+	const scalar_t    _scale = scale[j];
+
+	W_r[i]       = (scalar_t((_Wq & 0x80) >> 7) - _zero)*_scale;  //1st chunk
+	W_r[i + n]   = (scalar_t((_Wq & 0x40) >> 6) - _zero)*_scale;  //2nd chunk
+	W_r[i + n*2] = (scalar_t((_Wq & 0x20) >> 5) - _zero)*_scale;  //3rd chunk	
+	W_r[i + n*3] = (scalar_t((_Wq & 0x10) >> 4) - _zero)*_scale;  //4th chunk	
+	W_r[i + n*4] = (scalar_t((_Wq & 0x08) >> 3) - _zero)*_scale;  //5th chunk	
+	W_r[i + n*5] = (scalar_t((_Wq & 0x04) >> 2) - _zero)*_scale;  //6th chunk	
+	W_r[i + n*6] = (scalar_t((_Wq & 0x02) >> 1) - _zero)*_scale;  //7th chunk	
+	W_r[i + n*7] = (scalar_t((_Wq & 0x01))      - _zero)*_scale;  //8th chunk	
 }
-
-// //Shared
-// template <typename scalar_t>
-// __global__ void dequantize_1bit_u8_kernel(unsigned char* Wq_packed, scalar_t* scale, scalar_t* zero, scalar_t* W_r, int h, int w) { 
-// 	int i = blockIdx.x*blockDim.x + threadIdx.x;
-// 	int s = threadIdx.x;
-// 	int n = h*w;
-// 	if(i>=n) return;
-
-// 	__shared__ unsigned char shared[BLOCK_SIZE];
-// 	__shared__ scalar_t shared_meta[BLOCK_SIZE][2];
-	
-// 	int j             = i % w;
-// 	shared[s]         = Wq_packed[i];
-// 	shared_meta[s][0] = zero[j];
-// 	shared_meta[s][1] = scale[j];
-// 	__syncthreads();
-
-// 	W_r[i]       = (scalar_t((shared[s] & 0x80) >> 7) - shared_meta[s][0])*shared_meta[s][1]; //1st chunk
-// 	W_r[i + n]   = (scalar_t((shared[s] & 0x40) >> 6) - shared_meta[s][0])*shared_meta[s][1]; //2nd chunk
-// 	W_r[i + n*2] = (scalar_t((shared[s] & 0x20) >> 5) - shared_meta[s][0])*shared_meta[s][1]; //3rd chunk	
-// 	W_r[i + n*3] = (scalar_t((shared[s] & 0x10) >> 4) - shared_meta[s][0])*shared_meta[s][1]; //4th chunk	
-// 	W_r[i + n*4] = (scalar_t((shared[s] & 0x08) >> 3) - shared_meta[s][0])*shared_meta[s][1]; //5th chunk	
-// 	W_r[i + n*5] = (scalar_t((shared[s] & 0x04) >> 2) - shared_meta[s][0])*shared_meta[s][1]; //6th chunk	
-// 	W_r[i + n*6] = (scalar_t((shared[s] & 0x02) >> 1) - shared_meta[s][0])*shared_meta[s][1]; //7th chunk	
-// 	W_r[i + n*7] = (scalar_t((shared[s] & 0x01))      - shared_meta[s][0])*shared_meta[s][1]; //8th chunk	
-// }
 
 
 torch::Tensor dequantize_1bit_u8(torch::Tensor &Wq_packed, torch::Tensor &scale, torch::Tensor &zero)
@@ -370,11 +320,8 @@ torch::Tensor dequantize_1bit_u8(torch::Tensor &Wq_packed, torch::Tensor &scale,
 
 
 	C10_CUDA_KERNEL_LAUNCH_CHECK();
-	//cudaDeviceSynchronize();
-
 	return W_r;
 }
-
 
 
 /*******************************************************************************************************************************************/
@@ -382,20 +329,22 @@ torch::Tensor dequantize_1bit_u8(torch::Tensor &Wq_packed, torch::Tensor &scale,
 /*******************************************************************************************************************************************/
 
 //Simple
-__global__ void unpack_3bit_32_kernel(int32_t* Wq_packed, unsigned char* Wq_unpacked, int n) { 
+__global__ void unpack_3bit_32_kernel(int32_t* __restrict__ Wq_packed, unsigned char* Wq_unpacked, int n) { 
 	int i = blockIdx.x*blockDim.x + threadIdx.x;
 	if(i>=n) return;
 
-	Wq_unpacked[i]       = (Wq_packed[i] & 0x38000000) >> 27;  //1st chunk
-	Wq_unpacked[i + n]   = (Wq_packed[i] & 0x07000000) >> 24;  //2nd chunk
-	Wq_unpacked[i + n*2] = (Wq_packed[i] & 0x00E00000) >> 21;  //3rd chunk	
-	Wq_unpacked[i + n*3] = (Wq_packed[i] & 0x001C0000) >> 18;  //4th chunk	
-	Wq_unpacked[i + n*4] = (Wq_packed[i] & 0x00038000) >> 15;  //5th chunk	
-	Wq_unpacked[i + n*5] = (Wq_packed[i] & 0x00007000) >> 12;  //6th chunk	
-	Wq_unpacked[i + n*6] = (Wq_packed[i] & 0x00000E00) >> 9;   //7th chunk	
-	Wq_unpacked[i + n*7] = (Wq_packed[i] & 0x000001C0) >> 6;   //8th chunk	
-	Wq_unpacked[i + n*8] = (Wq_packed[i] & 0x00000038) >> 3;   //9th chunk	
-	Wq_unpacked[i + n*9] = (Wq_packed[i] & 0x00000007);        //10th chunk	
+	const int32_t  _Wq   = Wq_packed[i];
+
+	Wq_unpacked[i]       = (_Wq & 0x38000000) >> 27;  //1st chunk
+	Wq_unpacked[i + n]   = (_Wq & 0x07000000) >> 24;  //2nd chunk
+	Wq_unpacked[i + n*2] = (_Wq & 0x00E00000) >> 21;  //3rd chunk	
+	Wq_unpacked[i + n*3] = (_Wq & 0x001C0000) >> 18;  //4th chunk	
+	Wq_unpacked[i + n*4] = (_Wq & 0x00038000) >> 15;  //5th chunk	
+	Wq_unpacked[i + n*5] = (_Wq & 0x00007000) >> 12;  //6th chunk	
+	Wq_unpacked[i + n*6] = (_Wq & 0x00000E00) >> 9;   //7th chunk	
+	Wq_unpacked[i + n*7] = (_Wq & 0x000001C0) >> 6;   //8th chunk	
+	Wq_unpacked[i + n*8] = (_Wq & 0x00000038) >> 3;   //9th chunk	
+	Wq_unpacked[i + n*9] = (_Wq & 0x00000007);        //10th chunk	
 }
 
 
@@ -415,30 +364,32 @@ torch::Tensor unpack_3bit_32(torch::Tensor &Wq_packed)
 	int blocks = cdiv(n, BLOCK_SIZE);
 	unpack_3bit_32_kernel<<<blocks, BLOCK_SIZE>>>(Wq_packed.data_ptr<int32_t>(), Wq_unpacked.data_ptr<unsigned char>(), n);	
 
-	C10_CUDA_KERNEL_LAUNCH_CHECK();
-	//cudaDeviceSynchronize();
-	
+	C10_CUDA_KERNEL_LAUNCH_CHECK();	
 	return Wq_unpacked;
 }
 
 //Simple
 template <typename scalar_t>
-__global__ void dequantize_3bit_32_kernel(int32_t* Wq_packed, scalar_t* scale, scalar_t* zero, scalar_t* W_r, int h, int w) { 
+__global__ void dequantize_3bit_32_kernel(int32_t* __restrict__ Wq_packed, scalar_t* __restrict__ scale, scalar_t* __restrict__ zero, scalar_t* W_r, int h, int w) { 
 	int i = blockIdx.x*blockDim.x + threadIdx.x;
-	int n = h*w;
+	const int n = h*w;
 	if(i>=n) return;
 
-	int j        = i % w;
-	W_r[i]       = (scalar_t((Wq_packed[i] & 0x38000000) >> 27) - zero[j])*scale[j];  //1st chunk
-	W_r[i + n]   = (scalar_t((Wq_packed[i] & 0x07000000) >> 24) - zero[j])*scale[j];  //2nd chunk
-	W_r[i + n*2] = (scalar_t((Wq_packed[i] & 0x00E00000) >> 21) - zero[j])*scale[j];  //3rd chunk	
-	W_r[i + n*3] = (scalar_t((Wq_packed[i] & 0x001C0000) >> 18) - zero[j])*scale[j];  //4th chunk	
-	W_r[i + n*4] = (scalar_t((Wq_packed[i] & 0x00038000) >> 15) - zero[j])*scale[j];  //5th chunk	
-	W_r[i + n*5] = (scalar_t((Wq_packed[i] & 0x00007000) >> 12) - zero[j])*scale[j];  //6th chunk	
-	W_r[i + n*6] = (scalar_t((Wq_packed[i] & 0x00000E00) >> 9)  - zero[j])*scale[j];  //7th chunk	
-	W_r[i + n*7] = (scalar_t((Wq_packed[i] & 0x000001C0) >> 6)  - zero[j])*scale[j];  //8th chunk	
-	W_r[i + n*8] = (scalar_t((Wq_packed[i] & 0x00000038) >> 3)  - zero[j])*scale[j];  //9th chunk	
-	W_r[i + n*9] = (scalar_t((Wq_packed[i] & 0x00000007))       - zero[j])*scale[j];  //10th chunk	
+	const int j           = i % w;
+	const int32_t  _Wq    = Wq_packed[i];
+	const scalar_t _zero  = zero[j];
+	const scalar_t _scale = scale[j];
+
+	W_r[i]       = (scalar_t((_Wq & 0x38000000) >> 27) - _zero)*_scale;  //1st chunk
+	W_r[i + n]   = (scalar_t((_Wq & 0x07000000) >> 24) - _zero)*_scale;  //2nd chunk
+	W_r[i + n*2] = (scalar_t((_Wq & 0x00E00000) >> 21) - _zero)*_scale;  //3rd chunk	
+	W_r[i + n*3] = (scalar_t((_Wq & 0x001C0000) >> 18) - _zero)*_scale;  //4th chunk	
+	W_r[i + n*4] = (scalar_t((_Wq & 0x00038000) >> 15) - _zero)*_scale;  //5th chunk	
+	W_r[i + n*5] = (scalar_t((_Wq & 0x00007000) >> 12) - _zero)*_scale;  //6th chunk	
+	W_r[i + n*6] = (scalar_t((_Wq & 0x00000E00) >> 9)  - _zero)*_scale;  //7th chunk	
+	W_r[i + n*7] = (scalar_t((_Wq & 0x000001C0) >> 6)  - _zero)*_scale;  //8th chunk	
+	W_r[i + n*8] = (scalar_t((_Wq & 0x00000038) >> 3)  - _zero)*_scale;  //9th chunk	
+	W_r[i + n*9] = (scalar_t((_Wq & 0x00000007))       - _zero)*_scale;  //10th chunk	
 }
 
 torch::Tensor dequantize_3bit_32(torch::Tensor &Wq_packed, torch::Tensor &scale, torch::Tensor &zero)
@@ -468,8 +419,5 @@ torch::Tensor dequantize_3bit_32(torch::Tensor &Wq_packed, torch::Tensor &scale,
 
 
 	C10_CUDA_KERNEL_LAUNCH_CHECK();
-	//cudaDeviceSynchronize();
-
 	return W_r;
-
 }
