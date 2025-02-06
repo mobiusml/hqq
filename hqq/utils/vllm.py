08c2d0f8d3ce2c166de5f47c47efb13509b95600
mobicham <hicham@mobiuslabs.com>
2025-02-06 15:51:30 +0000

    fix logging

--- hqq/utils/vllm.py
+++ hqq/utils/vllm.py
@@ -1,6 +1,6 @@
 ####################################################
 from typing import Any, Dict, List, Optional
-import torch, numpy, logger
+import torch, numpy, logging
 
 from vllm import _custom_ops as ops
 from vllm.logger import init_logger
