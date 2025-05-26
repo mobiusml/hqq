a2241e3c8aac4e94b622206850e9341c00b724e1
mobicham <37179323+mobicham@users.noreply.github.com>
2025-05-26 18:58:56 +0200

    Update hqq_lib_demo.py

--- examples/hqq_lib_demo.py
+++ examples/hqq_lib_demo.py
@@ -1,11 +1,10 @@
 # pip install git+https://github.com/mobiusml/hqq.git;
 # pip install git+https://github.com/mobiusml/gemlite.git; #to use the gemlite backend
-# pip install bitblas; #to use the bitblas backend
 # OMP_NUM_THREADS=8 CUDA_VISIBLE_DEVICES=0 ipython3;
 ########################################################################
 import torch
 device        = 'cuda:0'
-backend       = "gemlite" #'torchao_int4' #"torchao_int4" (4-bit only) or "bitblas" (4-bit + 2-bit) or "gemlite" (8-bit, 4-bit, 2-bit, 1-bit)
+backend       = "torchao_int4" #'torchao_int4' #"torchao_int4" (4-bit only) or "bitblas" (4-bit + 2-bit) or "gemlite" (8-bit, 4-bit, 2-bit, 1-bit)
 compute_dtype = torch.bfloat16 if backend=="torchao_int4" else torch.float16
 cache_dir     = None
 model_id      = 'meta-llama/Meta-Llama-3-8B-Instruct'
